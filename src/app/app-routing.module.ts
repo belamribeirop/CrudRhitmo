c3e8ed51a2440492c8fcc9d9c560c6290412ed56
Isabela Ribeiro <belamribeirop@gmail.com>
2022-11-03 12:55:33 -0300

    import modules and add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditClientComponent } from './client/edit-client/edit-client.component';
+import { ListClientComponent } from './client/list-client/list-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list-client', pathMatch: 'full' },
+  { path: 'list-client', component: ListClientComponent },
+  { path: 'edit-client', component: EditClientComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  DeleteDialog,
+  ListClientComponent,
+} from './client/list-client/list-client.component';
+import { EditClientComponent } from './client/edit-client/edit-client.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogModule } from '@angular/cdk/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListClientComponent,
+    EditClientComponent,
+    FooterComponent,
+    TopBarComponent,
+    DeleteDialog,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatTableModule,
+    HttpClientModule,
+    CdkTableModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatDialogModule,
+    DialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
